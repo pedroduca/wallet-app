c7ab7fa355cf61191865d0ac24573a384be52001
Pedro Duca <testandoduca@gmail.com>
2024-10-07 20:36:07 -0300

    feat: Update App.tsx to use custom fonts and handle splash screen hiding

--- App.tsx
+++ App.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './src/store';
-import Routes from './src/routes';
+import React, { useCallback } from 'react'
+import { Provider } from 'react-redux'
+import * as SplashScreen from 'expo-splash-screen'
+import {
+  PTSans_400Regular,
+  PTSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/pt-sans'
+import { View } from 'react-native'
 
+import Routes from './src/routes'
+import { store } from './src/store'
 
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PTSans_400Regular,
+    PTSans_700Bold,
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  );
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </View>
+  )
 }
