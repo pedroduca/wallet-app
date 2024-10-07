505f45bcd8419e21b9fc42f2f57029b61a8b1ecb
Pedro Duca <testandoduca@gmail.com>
2024-10-07 20:35:23 -0300

    feat: Add theme colors and typography styles

---
+++ src/theme.ts
@@ -0,0 +1,63 @@
+export const colors = {
+  //text-color
+  black: '#111',
+  grey: '#BBBBBB',
+  greyDark: '#3F3F3F',
+
+  //base-color
+  white: '#FFFFFF',
+  blueDark: '#142995',
+  greenLight: '#A5FF32',
+  blueLight: '#12C2E9',
+  greyLight: '#EEEEEE',
+
+  //alert-color
+  red: '#FF0000',
+  yellow: '#FFFF00',
+  green: '#00FF00',
+}
+
+export const typography = {
+  h1: {
+    fontSize: 28,
+    lineHeight: 32,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+  h2: {
+    fontSize: 26,
+    lineHeight: 28,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+  h3: {
+    fontSize: 22,
+    lineHeight: 24,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+  h4: {
+    fontSize: 20,
+    lineHeight: 22,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+  h5: {
+    fontSize: 18,
+    lineHeight: 20,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+  p: {
+    fontSize: 16,
+    lineHeight: 18,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+  pSmall: {
+    fontSize: 14,
+    lineHeight: 16,
+    fontFamily: 'PTSans_400Regular',
+    color: colors.greyDark,
+  },
+}
