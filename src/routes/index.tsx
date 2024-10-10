057a701defdac691bdd44526f2d847ad191173fa
Pedro Duca <testandoduca@gmail.com>
2024-10-10 02:19:49 -0300

    feat: add screens to route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/Home';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-const Stack = createStackNavigator();
+import Home from '../screens/Home'
+import MyCards from '../screens/MyCards'
+
+const Stack = createStackNavigator()
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTransparent: true,
+            headerShown: false,
+            title: '',
+          }}
+        />
+        <Stack.Screen
+          name="MyCards"
+          component={MyCards}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
